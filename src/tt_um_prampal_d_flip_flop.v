0f248bdd84ba5cafe1593c4e9470307d7c21d4a5
prampal <93888290+prampal@users.noreply.github.com>
2025-12-06 00:29:05 -0500

    Update tt_um_prampal_d_flip_flop.v

--- src/tt_um_prampal_d_flip_flop.v
+++ src/tt_um_prampal_d_flip_flop.v
@@ -4,7 +4,7 @@
  */
 
 `default_nettype none
-    module tt_um_prampal_d_flip_flop (
+module tt_um_prampal_d_flip_flop (
     input wire [7:0] ui_in, // Dedicated inputs
     output wire [7:0] uo_out, // Dedicated outputs
     input wire [7:0] uio_in, // IOs: Input path
